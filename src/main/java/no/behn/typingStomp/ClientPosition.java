db15af174d10a8630013f0e72a7686a159fc28ab
William Behn <behnwilliam@icloud.com>
2024-06-27 14:22:43 +0200

    Refactoring

---
+++ src/main/java/no/behn/typingStomp/ClientPosition.java
@@ -0,0 +1,17 @@
+package no.behn.typingStomp;
+
+public class ClientPosition {
+  private String content;
+
+  public ClientPosition() {
+  }
+
+  public ClientPosition(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+}
