f3369f170c56d2a2541d3deeac1e439b4e0c0cfa
William Behn <behnwilliam@icloud.com>
2024-07-10 01:11:27 +0200

    Remove unnecessary code from PositionController

--- src/main/java/no/behn/typingStomp/PositionController.java
+++ src/main/java/no/behn/typingStomp/PositionController.java
@@ -1,39 +1,24 @@
 package no.behn.typingStomp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.util.Map;
 
 
 @Controller
 public class PositionController {
-    private int playerCount = 0;
-
     private final RoomService roomService;
 
     @Autowired
     public PositionController(RoomService roomService) {
         this.roomService = roomService;
     }
-    @EventListener
-    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-        playerCount++;
-
-    }
-    
-    @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {  
-       playerCount--;
-    }
-
+   
     @MessageMapping("/room/{roomId}")
     @SendTo("/topic/room/{roomId}/positions")
     public Map<String, Integer> handlePosition(@DestinationVariable String roomId, String message, StompHeaderAccessor headerAccessor) {
